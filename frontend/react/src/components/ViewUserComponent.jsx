907ad43376833a3f6d85b27bd396731432cc3dc6
Koulik <94155066+Koulikinesis@users.noreply.github.com>
2023-05-21 20:59:02 +0530

    Update ViewUserComponent.jsx

--- frontend/react/src/components/ViewUserComponent.jsx
+++ frontend/react/src/components/ViewUserComponent.jsx
@@ -22,7 +22,7 @@ class ViewUserComponent extends Component {
             <div>
                 <br></br>
                 <div className = "card col-md-6 offset-md-3">
-                    <h3 className = "text-center"> View User Details</h3>
+                    <h3 className = "text-center"> View Task Details</h3>
                     <div className = "card-body">
                         <div className = "row">
                             <label> Task Name: </label>
